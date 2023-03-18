fc54fc0296406c4335c49434ff25c92b7a89150f
Qasim162000 <m.qasim162000@gmail.com>
2023-03-18 23:22:13 +0500

    Initializing project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+interface AppProps {}
+
+export const App: React.FC<AppProps> = ({}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-3xl font-bold text-center underline mt-8">
+        Hello world!
+      </h1>
+    </>
   );
-}
-
-export default App;
+};
