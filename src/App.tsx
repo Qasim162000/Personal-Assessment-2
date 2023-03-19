151af542b0fc0cde55a0b4564b333017d6d6a5f2
Qasim162000 <m.qasim162000@gmail.com>
2023-03-19 12:41:36 +0500

    halfway Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-import React from "react";
-import { Grid } from "./components/Grid";
+import { useState } from "react";
+import CSVReader from "react-csv-reader";
 
-interface AppProps {}
+const App = () => {
+  const [csvData, setCsvData] = useState([]);
 
-export const App: React.FC<AppProps> = ({}) => {
+  const handleFileUpload = (data: any, fileInfo: any) => {
+    setCsvData(data);
+  };
+
   return (
-    <>
-      <h1 className="text-3xl font-bold text-center underline mt-8">
-        Pie Graph here
-      </h1>
-      <Grid />
-    </>
+    <div className="container mx-auto py-10">
+      <CSVReader onFileLoaded={handleFileUpload} />
+      <table className="w-full border-collapse">
+        <thead>
+          <tr className="bg-gray-200">
+            <th className="border p-2">Column 1</th>
+            <th className="border p-2">Column 2</th>
+            <th className="border p-2">Column 3</th>
+          </tr>
+        </thead>
+        <tbody>
+          {csvData.map((row, i) => (
+            <tr key={i}>
+              <td className="border p-2">{row[0]}</td>
+              <td className="border p-2">{row[1]}</td>
+              <td className="border p-2">{row[2]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
+
+export default App;
